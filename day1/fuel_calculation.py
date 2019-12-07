25966129231a4cf40dc6b94aeedc7b4ba0913ac9
Sejal <sejal.dua@tufts.edu>
2019-12-07 16:02:06 +0100

    solved day1 puzzle 2

--- day1/fuel_calculation.py
+++ day1/fuel_calculation.py
@@ -1,12 +1,20 @@
-def quick_maths(fuel):
+# arithmetic function for puzzle 1
+def quick_maths1(fuel):
         return (fuel // 3) - 2
 
+# recursive function for puzzle 2
+def quick_maths2(fuel):
+        if fuel < 9:
+                return 0
+        return (fuel // 3) - 2 + quick_maths2((fuel // 3) - 2)
+
+# compute fuel needed for each input, then return the sum of these fuel requirements
 def calculate_fuel(fpath):
         total_fuel = 0
         with open(fpath, "r") as fp:
                 num = int(fp.readline())
                 while num:
-                        total_fuel += quick_maths(num)
+                        total_fuel += quick_maths2(num)
                         try:
                                 num = int(fp.readline())
                         except:
