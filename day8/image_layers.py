688c13f4222528b2dedaeeecd9d4109a97d78f10
Sejal <sejal.dua@tufts.edu>
2019-12-13 12:06:48 +0100

    solved day8 puzzle 2

--- day8/image_layers.py
+++ day8/image_layers.py
@@ -1,16 +1,44 @@
-def solve_part_1(img, width, height):
-    layer_sizes = [width * height * i for i in range(int(len(img) / (width * height)))]
-    min_zeroes = int(len(img) / height)
-    temp_answer = 0
+from collections import defaultdict
+
+def get_layers(img, width, height):
+    layers = defaultdict(list)
+    layer_sizes = [width * height * i for i in range(int(len(img) / (width * height) + 1))]
+    layer = 0
     for i in range(len(layer_sizes)-1):
-        curr_layer_counts = [0, 0, 0]
         for char in img[layer_sizes[i]:layer_sizes[i+1]]:
-            curr_layer_counts[int(char)] += 1
-        if curr_layer_counts[0] <= min_zeroes:
-            min_zeroes = curr_layer_counts[0]
-            temp_answer = curr_layer_counts[1] * curr_layer_counts[2]
-    return temp_answer
+            layers[layer].append(int(char))
+        layer += 1
+    return layers
+
+def print_image(layers, width, height):
+    for y in range(height):
+        for x in range(width):
+            idx = x + y * width
+            for l in range(len(layers)):
+                # transparent pixel
+                if layers[l][idx] == 2:
+                    continue
+                # white pixel
+                elif layers[l][idx] == 1:
+                    print("*", end="")
+                # black pixel
+                else:
+                    print(" ", end="")
+                break
+        print()
 
 if __name__ == "__main__":
+
+    # read image from input file and extract layers
     img = open('input.txt', 'r').read()[:-1]
-    print(solve_part_1(img, 25, 6))
+    width = 25
+    height = 6
+    layers = get_layers(img, width, height)
+
+    # PART 1: find layer with fewest zeroes and return number of 1s * number of 2s
+    zero_counts  = [pixels.count(0) for pixels in layers.values()]
+    result_layer = zero_counts.index(min(zero_counts))
+    print(layers[result_layer].count(1) * layers[result_layer].count(2))
+
+    # PART 2: decode message
+    print_image(layers, width, height)
